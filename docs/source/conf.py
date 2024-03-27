7f7ad77bdd66052fa01ddd88c5286ee199aa2d83
Iván Dimitri Novomiast Dieguez <idimnovdie1602@protonmail.com>
2024-03-27 10:06:19 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'cursus-42'
+copyright = '2024, INovomiast2'
+author = 'INovomiast2'
 
 release = '0.1'
-version = '0.1.0'
+version = '1.0.0'
 
 # -- General configuration
 
